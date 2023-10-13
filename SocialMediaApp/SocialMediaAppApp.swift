ccb7f384a750bfd5f38a02972d989d8559dcad08
Rishop Babu <rishopbabu22@gmail.com>
2023-10-13 17:25:22 +0530

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Mac-OBS-51 on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
